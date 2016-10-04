6436224fda03b6c9cf5767db9a5a827131bcf487
Victor Lazov <v.lazov@up2.nl>
2016-10-04 11:27:03 +0300

    Improvements ...

--- high-quality-methods/v.lazov/methods.php
+++ high-quality-methods/v.lazov/methods.php
@@ -1,79 +1,137 @@
 <?php
 
+/**
+ * Class Helper
+ */
 class Helper {
-	static calculate_triangle_area($a, $b, $c) {
-		if ($a <= 0 || $b <= 0 || $c <= 0) {
-			return "Sides should be positive numbers.";
-		}
-		
-		$perimeter = ($a + $b + $c) / 2;
-		$area = sqrt($perimeter * ($perimeter - $a) * (perimeter - $b) * ($perimeter - $c));
-		
-		return $area;
-	}
+  /**
+   * Calculates the area of a triangle based on 3 sides.
+   *
+   * @param $a
+   * @param $b
+   * @param $c
+   *
+   * @return float|string The area of the triangle
+   */
+  static function calculate_triangle_area($a, $b, $c) {
+    if ($a <= 0 || $b <= 0 || $c <= 0) {
+      return "Sides should be positive numbers.";
+    }
 
-	static digit_to_string($number) {
-		switch ($number) {
-			case 0: return "zero";
-			case 1: return "one";
-			case 2: return "two";
-			case 3: return "three";
-			case 4: return "four";
-			case 5: return "five";
-			case 6: return "six";
-			case 7: return "seven";
-			case 8: return "eight";
-			case 9: return "nine";
-		}
+    $perimeter = ($a + $b + $c) / 2;
+    $area = sqrt($perimeter * ($perimeter - $a) * ($perimeter - $b) * ($perimeter - $c));
 
-		return "Invalid digit!";
-	}
+    return $area;
+  }
 
-	static find_max_number($elements) {
-		if (!$elements || empty(elements)) {
-			return "The elements array is empty or does not exist.";
-		}
-		
-		$max = $elements[0];
+  /**
+   * Outputs the name of a given number.
+   *
+   * @param $number
+   *    A number
+   *
+   * @return string
+   *    The name of the given number or error string.
+   */
+  static function digit_to_string($number) {
+    switch ($number) {
+      case 0:
+        return "zero";
+      case 1:
+        return "one";
+      case 2:
+        return "two";
+      case 3:
+        return "three";
+      case 4:
+        return "four";
+      case 5:
+        return "five";
+      case 6:
+        return "six";
+      case 7:
+        return "seven";
+      case 8:
+        return "eight";
+      case 9:
+        return "nine";
+    }
 
-		for ($i = 1; $i < count($elements) - 1; $i++) {
-			if (elements[$i] > $max) {
-				$max = $elements[$i];
-			}
-		}
-		
-		return $max;
-	}
+    return "Invalid digit!";
+  }
 
-	static print_formatted_number($number, $format) {
-		if(!is_numeric($number)) {
-			return "You must pass an number!";
-		}
-		
-		switch ($format) {
-			case "f":
-				return number_format($number, 2, ',', ' ');
-			case "%":
-				return "Another number format.";
-			case "r":
-				return "Yet another number format.";
-			default:
-				return "Format not defined.";
-		}
-	}
+  /**
+   * Finds the maximum number in array.
+   *
+   * @param $elements
+   *    Array of numbers
+   *
+   * @return string
+   *    The maximum number in the array or an error string.
+   */
+  static function find_max_number($elements) {
+    if (!$elements || empty($elements)) {
+      return "The elements array is empty or does not exist.";
+    }
 
-	static calculate_distance($x1, $y1, $x2, $y2) {
-		/*
-		WTF C# shit ...
-		isHorizontal = (y1 == y2);
-		isVertical = (x1 == x2); */
+    $max = $elements[0];
 
-		return distance = sqrt(($x2 - $x1) * ($x2 - $x1) + ($y2 - $y1) * ($y2 - $y1));
-	}
+    for ($i = 1; $i < count($elements) - 1; $i++) {
+      if ($elements[$i] > $max) {
+        $max = $elements[$i];
+      }
+    }
+
+    return $max;
+  }
+
+  /**
+   * Prints a formatted number
+   *
+   * @param $number
+   * @param $format
+   *
+   * @return string
+   */
+  static function print_formatted_number($number, $format) {
+    if (!is_numeric($number)) {
+      return "You must pass an number!";
+    }
+
+    switch ($format) {
+      case "f":
+        return number_format($number, 2, ',', ' ');
+      case "%":
+        return "Another number format.";
+      case "r":
+        return "Yet another number format.";
+      default:
+        return "Format not defined.";
+    }
+  }
+
+  /**
+   * Calculates the distance between two points.
+   *
+   * @param $x1
+   * @param $y1
+   * @param $x2
+   * @param $y2
+   *
+   * @return float
+   */
+  static function calculate_distance($x1, $y1, $x2, $y2) {
+    /*
+    WTF C# shit ...
+    isHorizontal = (y1 == y2);
+    isVertical = (x1 == x2); */
+
+    return $distance = sqrt(($x2 - $x1) * ($x2 - $x1) + ($y2 - $y1) * ($y2 - $y1));
+  }
 }
 
-function please_do_observe () {
-	echo Helper::calculate_triangle_area(3, 4, 5));
+function please_do_observe() {
+  echo Helper::calculate_triangle_area(3, 4, 5));
 	
 	echo Helper::digit_to_string(5);
 	
@@ -88,16 +146,16 @@ function please_do_observe () {
 	include "student.php";
 	
 	$student1 = new Student("Peter", "Ivanov");
-	$student1.other_info = "From Sofia";
-	$student1.birth_date = "17.03.1992";
+	$student1->other_info = "From Sofia";
+	$student1->birth_date = "17.03.1992";
 
 	$student2 = new Student("Stella", "Markova");
-	$student2.other_info = "From Vidin, gamer, high results";
-	$student2.birth_date = "03.11.1993";
+	$student2->other_info = "From Vidin, gamer, high results";
+	$student2->birth_date = "03.11.1993";
 	
-	if($student1->is_older_than($student2)) {
-		echo "{$student1->first_name} is older than {$student2->first_name}";
-	} else {
-		echo "{$student1->first_name} is not older than {$student2->first_name}";
-	}
+	if ($student1->is_older_than($student2)) {
+    echo "{$student1->first_name} is older than {$student2->first_name}";
+  } else {
+    echo "{$student1->first_name} is not older than {$student2->first_name}";
+  }
 }
--- high-quality-methods/v.lazov/student.php
+++ high-quality-methods/v.lazov/student.php
@@ -1,20 +1,31 @@
 <?php
 
+/**
+ * Class Student
+ */
 class Student {
-	public $first_name;
-	public $last_name;
-	public $other_info;
-	public $birth_date;
-	
-	public __constuct($first_name, $last_name) {
-		$this->firs_name = $first_name;
-		$this->last_name = $last_name;
-	}
+  public $first_name;
+  public $last_name;
+  public $other_info;
+  public $birth_date;
 
-	public is_older_than($student) {			
-		if(strtotime($this->birth_date) > strtotime($student->birth_date))
-			return TRUE;
-		
-		return FALSE;
-	}
+  public function __construct($first_name, $last_name) {
+    $this->first_name = $first_name;
+    $this->last_name = $last_name;
+  }
+
+  /**
+   * Checks if the current student is older than the passed one.
+   *
+   * @param Object $student
+   *
+   * @return bool
+   *    TRUE if the current user is older, FALSE otherwise.
+   */
+  public function is_older_than($student) {
+    if (strtotime($this->birth_date) > strtotime($student->birth_date))
+      return TRUE;
+
+    return FALSE;
+  }
 }
